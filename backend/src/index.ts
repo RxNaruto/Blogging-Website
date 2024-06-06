758543ef70fcb4172363d352b421bdaba59d9486
Rithk <rithkchaudhary86@gmail.com>
2024-06-06 17:16:03 +0530

    routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,6 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import {decode,sign,verify} from 'hono/jwt'
-
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
 
 const app = new Hono<{
   Bindings: {
@@ -11,85 +9,8 @@ const app = new Hono<{
   }
 }>()
 
-app.post('/api/v1/user/signup', async(c) => {
-  const body = await c.req.json();
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
+app.route("api/v1/user",userRouter);
+app.route("api/v1/blog",blogRouter);
 
-  }).$extends(withAccelerate())
-
-  try {
-    const user = await prisma.user.create({
-      data: {
-        username: body.username,
-        password: body.password,
-        name: body.name,
-  
-      }
-      
-    })
-    const jwt = await sign({
-      id: user.id
-    },c.env.JWT_SECRET)
-    return c.text(jwt)
-  } catch (e) {
-    c.status(411);
-    return c.text('Invalid')
-    
-  }
-  
-})
-
-app.post('/api/v1/user/signin', async(c) => {
-  const body = await c.req.json();
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-
-  }).$extends(withAccelerate())
-
-  try {
-    const user = await prisma.user.findFirst({
-      where: {
-        username: body.username,
-        password: body.password,
-        
-  
-      }
-      
-    })
-    if(!user){
-      c.status(403);
-      return c.json({
-        message: "Incorrect Credentails"
-      })
-    }
-    const jwt = await sign({
-      id: user.id
-    },c.env.JWT_SECRET)
-    return c.text(jwt)
-    
-  } catch (e) {
-    console.log(e);
-    c.status(411);
-      return c.text('Invalid')
-    
-  }
-})
-
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.post('/api/v1/blog/bulk', (c) => {
-  return c.text('Hello Hono!')
-})
 
 export default app
