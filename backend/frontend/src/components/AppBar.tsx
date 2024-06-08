492098c0492bdca972c86433a09ad29fcdc2ec1f
Rithk <rithkchaudhary86@gmail.com>
2024-06-08 23:16:55 +0530

    name change

--- backend/frontend/src/components/AppBar.tsx
+++ backend/frontend/src/components/AppBar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const AppBar=()=>{
     return <div className="border-b flex justify-between px-10 py-4">
         <div className="flex flex-col justify-center">
-            Medium
+           Vichar
         </div>
         <div>
             <Link to={'/publish'}>
